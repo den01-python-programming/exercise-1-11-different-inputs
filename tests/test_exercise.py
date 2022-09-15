897879d86260b72533bcbe8181d97c149514d990
graphics80 <K.Ilkow@gmail.com>
2022-09-15 12:46:36 +0200

    Update test_exercise.py
    Casting a String in a Boolean is always true

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -2,8 +2,8 @@ import pytest
 import src.exercise
 
 def test_exercise():
-    input_values = ["bye","114","4.6","False"]
-    input_values_stored = ["bye","114","4.6","False"]
+    input_values = ["bye","114","4.6"]
+    input_values_stored = ["bye","114","4.6"]
     output = []
 
     def mock_input(s=None):
@@ -19,8 +19,7 @@ def test_exercise():
 
     src.exercise.main()
 
-    assert [output[0],output[1],output[2],output[3]] == ["Give a string:","Give an integer:","Give a float:","Give a boolean:"]
-    assert [output[4],output[5],output[6],output[7]] == ["You gave the string " + input_values_stored[0],\
+    assert [output[0],output[1],output[2]]] == ["Give a string:","Give an integer:","Give a float:"]
+    assert [output[3],output[4],output[5]] == ["You gave the string " + input_values_stored[0],\
                                                          "You gave the integer " + input_values_stored[1],\
-                                                         "You gave the float " + input_values_stored[2],\
-                                                         "You gave the boolean " + input_values_stored[3]]
+                                                         "You gave the float " + input_values_stored[2]]
